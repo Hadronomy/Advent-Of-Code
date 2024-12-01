b527a1b4e0bd95701c4eb0a73c69afaf09cced2d
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-12-01 07:00:15 +0000

    fix: bad crate name in 2023 problem

--- 2023/day-6/src/bin/part1.rs
+++ 2023/day-6/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use aoc2024_day_6::part1;
+use aoc2023_day_6::part1;
 
 fn main() {
     let input = include_str!("input.txt");
--- 2023/day-6/src/bin/part2.rs
+++ 2023/day-6/src/bin/part2.rs
@@ -1,4 +1,4 @@
-use aoc2024_day_6::part2;
+use aoc2023_day_6::part2;
 
 fn main() {
     let input = include_str!("input.txt");
