6db79778525487baa34bdc44022a13a15e0ad51e
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-12-01 21:30:32 +0000

    feat: add `tracing-subscriber to binaries

--- 2024/day-1/src/bin/part1.rs
+++ 2024/day-1/src/bin/part1.rs
@@ -1,6 +1,7 @@
 use aoc2024_day_1::part1;
 
 fn main() {
+    tracing_subscriber::fmt::init();
     let input = include_str!("input.txt");
     let result = part1::process(input);
     println!("Result: {}", result.expect("should have a result"));
--- daily-template/src/bin/part2.rs
+++ daily-template/src/bin/part2.rs
@@ -1,6 +1,7 @@
 use {{crate_name}}::part2;
 
 fn main() {
+    tracing_subscriber::fmt::init();
     let input = include_str!("input.txt");
     let result = part2::process(input);
     println!("Result: {}", result.expect("should have a result"));
